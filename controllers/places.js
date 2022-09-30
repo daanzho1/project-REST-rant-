0770a4f8b448e78646f9814e849707321d767a7b
daanzho1 <barrywilliams9000@gmail.comgit config --global user.name Your>
2022-09-29 22:10:56 -0700

    jazzed up forms

--- controllers/places.js
+++ controllers/places.js
@@ -18,6 +18,21 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+router.post('/', (req, res) => {
+    //console.log(req.body)
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+})
 
 
 router.get('/new', (req, res) => {
